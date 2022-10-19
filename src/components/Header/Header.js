7392edd24a59a40ad52276247a035e7070fd31a6
unknown <hasinadilruba2021@gmail.com>
2022-10-19 19:25:42 +0200

    category component responsiveness done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,30 @@
 import React, { useState } from 'react';
-// import { Link, NavLink } from 'react-router-dom';
 import logo from '../../images/logo.svg'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Header.css'
 
 const Header = () => {
   return (
-    <div className='nav-container bg-warning header'>
-     <nav class="navbar navbar-expand-lg bg-warning container">   
-      <div className="container-fluid title">
-      <div className='title'>
-      <img id='logo' src={logo} alt="" />
-      <h3 className="navbar-brand">Tech Quizzes</h3>
-      </div>
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>   
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="/">Topics</a>
-        <a className="nav-link" href="/blog">Blog</a>
-        <a className="nav-link" href="/statistics">Statistics</a>
-        <a className="nav-link" href="/about">About</a>
-      </div>
-    </div>
-  </div>
-</nav>
-
-        {/* <nav className='header'>
+    <div className='nav-container bg-warning'>
+     <nav class="navbar navbar-expand-lg bg-warning container header">   
+       <div className="container-fluid title">
          <div className='title'>
-             <img id='logo' src={logo} alt="" />
-             <h3>Tech Quizzes</h3>
+           <img id='logo' src={logo} alt="" />
+           <h3 className="navbar-brand">Tech Quizzes</h3>
          </div>
-          <div>
-              <NavLink to="/">Topics</NavLink>
-              <NavLink to="/blog">Blog</NavLink>
-              <NavLink to="/statistics">Statistics</NavLink>
-              <NavLink to="/about">About</NavLink>
+             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+             <span className="navbar-toggler-icon"></span>
+            </button>   
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+             <a className="nav-link active" aria-current="page" href="/">Topics</a>
+             <a className="nav-link" href="/blog">Blog</a>
+             <a className="nav-link" href="/statistics">Statistics</a>
+             <a className="nav-link" href="/about">About</a>
           </div>
-        </nav>  */}
-     </div>    
+        </div>
+      </div>
+    </nav>
+   </div>    
   );
 };
 
